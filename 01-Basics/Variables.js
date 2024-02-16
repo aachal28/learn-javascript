60de721ba3e364656591cddb6b8c8e6e6ba026bb
Aachal Pardeshi <77334130+aachal28@users.noreply.github.com>
2024-02-16 15:50:53 +0000

    added config

---
+++ 01-Basics/Variables.js
@@ -0,0 +1,7 @@
+const accountId=12344
+let accountName="Aachal"
+var accountMail="Aachal@Github.com"
+// accountId =12345677;
+
+console.log(accountId);
+console.table([accountId, accountName, accountMail])
