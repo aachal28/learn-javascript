6e8a7403e5e0a59f3f56932fa33db8be79e79dd7
Aachal Pardeshi <77334130+aachal28@users.noreply.github.com>
2024-02-17 17:13:14 +0000

    summary datatype

---
+++ 01-Basics/datatypes-summary.js
@@ -0,0 +1,37 @@
+//primitive
+
+//7 type: string, number, boolean, null(empty), undefiened(space), symbol, BigInt
+
+const score =100;
+const scoreVal =100.3
+
+const isloggein=true;
+const outsidetemp = null // (Completely empty)
+let user;//(undefiened)
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+const bignumber = 284664547139436187n
+
+
+//Reference type / non primitive
+
+// array, objects, functions
+
+const heros = ["shaktiman", "doga"]
+let myobj = {
+    name:"aachal",
+    age: 23,
+}
+
+const myfunction= function()
+{
+    console.log("hello")
+}
+
+console.log(typeof bigNumber);
+
+//// https://262.ecma-international.org/5.1/#sec-11.4.3
