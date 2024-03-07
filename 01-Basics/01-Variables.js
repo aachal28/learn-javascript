b6179f99d4023568b2e675198ec9c8fde035629a
Aachal Pardeshi <77334130+aachal28@users.noreply.github.com>
2024-03-07 15:11:21 +0530

    Update 01-Variables.js

--- 01-Basics/01-Variables.js
+++ 01-Basics/01-Variables.js
@@ -12,6 +12,12 @@ let accountstate; // Declared but not assigned a value, defaults to undefined
 
 // Redefining the variable accountName, which is not allowed
 // let accountName = "sam";
+let Name = "Code Station";
+console.log(Name); // Output: Aachal
+
+Name = "Coding With Aachal"; // Reassigning the value of accountName
+console.log(Name); // Output: Sam
+console.log(accountId);
 
 // Redefining the variable accountMail
 var accountMail = "sam@Github.com";
@@ -24,18 +30,5 @@ becuase of issue in block scope and functional scope
 That's Why Let come to solve problem of scope block in early days
 */
 
-
 // Printing values in a table format
 console.table([accountId, accountName, accountMail, accountCity, accountstate, accountName, accountMail]);
-
-
-let Name = "Code Station";
-console.log(Name); // Output: Aachal
-
-Name = "Coding With Aachal"; // Reassigning the value of accountName
-console.log(Name); // Output: Sam
-console.log(accountId);
-
-
-
-
