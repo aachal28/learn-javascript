2c6f416312e6d646877357233f575a5a6f61eb78
Aachal Pardeshi <77334130+aachal28@users.noreply.github.com>
2024-03-07 15:24:58 +0530

    Update datatypes.js

--- 01-Basics/datatypes.js
+++ 01-Basics/datatypes.js
@@ -1,14 +1,14 @@
 "use strict"; //treat all js code as newer version
 
-// alert( 3+3); w are using node js not browser
+// alert( 3+3) we are using node js not browser
 
 console.log("hello")
-
 
 let name = "aachal"
 let age = 23
 let isLoggedIn = false
-let state;
+let state; // undefined
+state = null; // empty
 
 // number => 2 to power 53
 // bigint
